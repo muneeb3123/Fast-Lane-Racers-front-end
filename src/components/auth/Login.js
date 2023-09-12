a49d4f67c6a90e22522560310bc79e9a90e12b2e
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-09-12 20:18:35 +0500

    create fully functional login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,32 +1,48 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { toast } from 'react-hot-toast';
+import { useDispatch, useSelector } from 'react-redux';
+import { currentUser } from '../../redux/auth/currentUserSlice';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const user = useSelector((state) => state.signup.user);
-  console.log(user);
+  const { isUser } = useSelector((state) => state.currentUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {
-    console.log('hy');
+  useEffect(() => {
+    if (isUser) {
+      toast.error('You are already logged in');
+      navigate('/');
+    }
+  }, [isUser, navigate]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {
+      email,
+      password,
+    };
+    try {
+      const response = await axios.post('http://127.0.0.1:3000/login', {
+        user: formData,
+      });
+      localStorage.setItem('token', response.headers.authorization);
+      toast.success(response.data.message);
+      dispatch(currentUser());
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
   };
+
   return (
-    <>
-      {user ? (
-        <p>
-          Welcome,
-          {user.user.name}
-          !
-        </p>
-      ) : (
-        <p>Please log in or sign up.</p>
-      )}
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Register</button>
-      </form>
-    </>
+    <form onSubmit={handleSubmit}>
+      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
   );
 }
 
