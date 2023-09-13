d7511073fedcb058c5d288857290a41fb629ded5
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-09-13 00:43:13 -0400

    Added icons on reservation

--- src/component/CardCar.js
+++ src/component/CardCar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import facebook from '../images/facebook.svg';
+import twitter from '../images/twitter.svg';
+import instagram from '../images/instagram.svg';
 
 const CardCar = ({ name, description, image, color }) => {
   const backColor = {
@@ -17,13 +20,13 @@ return (
     <p>{description}</p>
     <div className='social-buttons'>
       <a href='www.facebook.com' className='social-button' target="_blank" rel="noopener noreferrer">
-        <i className='fab fa-facebook-f' />
+        <img src={facebook} alt='facebook logo'/>
       </a>
       <a href='www.teitter.com' className='social-button' target="_blank" rel="noopener noreferrer">
-        <i className='fab fa-twitter' />
+        <img src={twitter} alt='twitter logo'/>
       </a>
       <a href='www.instagram.com' className='social-button' target="_blank" rel="noopener noreferrer">
-        <i className='fab fa-instagram' />
+        <img src={instagram} alt='instagram logo'/>
       </a>
     </div>
   </section>
