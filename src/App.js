f23f23597afba7e95ea87f7a9c2b14c018a4eed4
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-09-12 23:29:38 -0400

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Reservation from './component/Reservation';
+import Reservation1 from './component/Reservation1';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hy</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/reservation1" element={<Reservation1 />} /> 
+      </Routes>
+    </Router>
   );
 }
 
