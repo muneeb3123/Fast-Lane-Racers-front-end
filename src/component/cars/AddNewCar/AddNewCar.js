287c495142846e18fbe98c76d93e45425eca4dde
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-09-18 20:29:44 +0500

    add slice to create-new-car

--- src/component/cars/AddNewCar/AddNewCar.js
+++ src/component/cars/AddNewCar/AddNewCar.js
@@ -1,20 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createNewCar } from '../../../redux/cars/AddNewCarSlice';
 import './AddNewCar.css';
 
 export default function AddNewCar() {
+  const dispatch = useDispatch();
+  const [carData, setCarData] = useState({
+    name: '',
+    finance_fee: 0,
+    option_to_purchase_fee: 0,
+    total_amount_payable: 0,
+    duration: '',
+    apr: 0.0,
+    image: '',
+    description: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createNewCar(carData))
+      .unwrap()
+      .then(() => {
+        // Handle success, e.g., show a success message or redirect
+        console.log('New car added successfully');
+      })
+      .catch((error) => {
+        // Handle error, e.g., show an error message
+        console.error('Error adding new car:', error);
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCarData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <section className="add-new-car-page">
       <h1 className="add-new-car-heading">Add New Car</h1>
 
-      <form className="add-new-car-form">
-        <input type="text" placeholder="Name" />
-        <input type="number" placeholder="Finance Fee" />
-        <input type="number" placeholder="Option To Purchase Fee" />
-        <input type="number" placeholder="Total Amount Payable" />
-        <input type="text" placeholder="Duration" />
-        <input type="number" placeholder="APR" step={0.01} />
-        <input type="text" placeholder="Image Link" />
-        <input type="textarea" placeholder="Description" className="description" />
+      <form className="add-new-car-form" onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" onChange={handleChange} />
+        <input type="number" name="finance_fee" placeholder="Finance Fee" onChange={handleChange} />
+        <input type="number" name="option_to_purchase_fee" placeholder="Option To Purchase Fee" onChange={handleChange} />
+        <input type="number" name="total_amount_payable" placeholder="Total Amount Payable" onChange={handleChange} />
+        <input type="text" name="duration" placeholder="Duration" onChange={handleChange} />
+        <input type="number" name="apr" placeholder="APR" step={0.01} onChange={handleChange} />
+        <input type="text" name="image" placeholder="Image Link" onChange={handleChange} />
+        <textarea
+          name="description"
+          placeholder="Description"
+          className="description"
+          onChange={handleChange}
+        />
         <button type="submit">Add New Car</button>
       </form>
     </section>
---
+++ src/redux/cars/AddNewCarSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Define the initial state for car creation
+const initialState = {
+  newCar: null,
+  status: 'idle',
+  error: null,
+};
+
+// Define the async thunk for creating a new car
+export const createNewCar = createAsyncThunk('cars/createNewCar', async (carData) => {
+  try {
+    const response = await axios.post('http://127.0.0.1:3000/cars', carData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data; // Throw the API error response
+  }
+});
+
+// Create a cars slice
+const createNewCarSlice = createSlice({
+  name: 'cars',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createNewCar.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(createNewCar.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.newCar = action.payload;
+      })
+      .addCase(createNewCar.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+// Export the async thunk and the reducer
+export default createNewCarSlice.reducer;
