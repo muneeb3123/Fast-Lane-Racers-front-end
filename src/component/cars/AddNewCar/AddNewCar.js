4a8105d461fffd25f589d31669cd42fcb9d5aa71
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-09-18 19:57:38 +0500

    Add styling to add-new-car component

--- src/component/cars/AddNewCar/AddNewCar.js
+++ src/component/cars/AddNewCar/AddNewCar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './AddNewCar.css';
 
 export default function AddNewCar() {
   return (
     <section className="add-new-car-page">
-      <h1>AddNewCar</h1>
+      <h1 className="add-new-car-heading">Add New Car</h1>
 
-      <form>
+      <form className="add-new-car-form">
         <input type="text" placeholder="Name" />
         <input type="number" placeholder="Finance Fee" />
         <input type="number" placeholder="Option To Purchase Fee" />
@@ -13,7 +14,7 @@ export default function AddNewCar() {
         <input type="text" placeholder="Duration" />
         <input type="number" placeholder="APR" step={0.01} />
         <input type="text" placeholder="Image Link" />
-        <input type="textarea" placeholder="Description" />
+        <input type="textarea" placeholder="Description" className="description" />
         <button type="submit">Add New Car</button>
       </form>
     </section>
